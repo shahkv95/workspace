940409897ab12c6a2a6d404228790ff4d795c4f5
shahkv95 <kushshah253@gmail.com>
2023-03-04 20:13:19 +0530

    extended functionality to save model weights for logitic regression

--- src/models/logistic_regression.py
+++ src/models/logistic_regression.py
@@ -1,10 +1,12 @@
+import joblib
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 from typing import Tuple
+import os
 
 
 def fit_logistic_regression_model(
-    X_train: Tuple, X_test: Tuple, y_train: Tuple, y_test: Tuple
+    X_train: Tuple, X_test: Tuple, y_train: Tuple, y_test: Tuple, model_name: str
 ) -> None:
     """
     Fit logistic regression model on the training set, make predictions on the test set, and print evaluation metrics.
@@ -23,3 +25,11 @@ def fit_logistic_regression_model(
 
     report = classification_report(y_test, predictions)
     print(f"Classification Report:\n{report}")
+
+    # Save model weights
+    model_weights_dir = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "model_weights"
+    )
+    os.makedirs(model_weights_dir, exist_ok=True)
+    weights_file_path = os.path.join(model_weights_dir, f"{model_name}_weights.joblib")
+    joblib.dump(model.coef_, weights_file_path)
