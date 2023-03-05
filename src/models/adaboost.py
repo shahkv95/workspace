f701113fb3c2c84374c1bab6da41ca4aad56a6d1
shahkv95 <kushshah253@gmail.com>
2023-03-05 11:50:01 +0530

    Setup logging and exception handling for adaboost

--- src/models/adaboost.py
+++ src/models/adaboost.py
@@ -1,36 +1,63 @@
+import logging
+import os
+from typing import Tuple
+
 import joblib
+import numpy as np
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
-from typing import Tuple
-import os
+
+
+logging.basicConfig(level=logging.INFO)
 
 
 def fit_adaboost_model(
-    X_train: Tuple, X_test: Tuple, y_train: Tuple, y_test: Tuple, model_name: str
+    X_train: np.ndarray,
+    X_test: np.ndarray,
+    y_train: np.ndarray,
+    y_test: np.ndarray,
+    model_name: str,
 ) -> None:
     """
     Fit Adaboost classifier on the training set, make predictions on the test set, and print evaluation metrics.
+
+    Args:
+        X_train (np.ndarray): Training input features.
+        X_test (np.ndarray): Test input features.
+        y_train (np.ndarray): Training target labels.
+        y_test (np.ndarray): Test target labels.
+        model_name (str): Name of the model to be saved.
+
+    Returns:
+        None
     """
+    try:
+        logging.info("\n============= ADABOOST MODEL =============\n")
 
-    print("\n================    ADABOOST MODEL    ================\n")
-    model = AdaBoostClassifier()
-    model.fit(X_train, y_train)
+        model = AdaBoostClassifier()
+        model.fit(X_train, y_train)
 
-    predictions = model.predict(X_test)
+        predictions = model.predict(X_test)
 
-    accuracy = accuracy_score(y_test, predictions)
-    print(f"Accuracy: {accuracy:.2%}")
+        accuracy = accuracy_score(y_test, predictions)
+        logging.info(f"\nAccuracy: {accuracy:.2%}")
 
-    cm = confusion_matrix(y_test, predictions)
-    print(f"Confusion Matrix:\n{cm}")
+        cm = confusion_matrix(y_test, predictions)
+        logging.info(f"\nConfusion Matrix:\n{cm}")
 
-    report = classification_report(y_test, predictions)
-    print(f"Classification Report:\n{report}")
+        report = classification_report(y_test, predictions)
+        logging.info(f"\nClassification Report:\n{report}")
 
-    # Save model weights
-    model_weights_dir = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), "model_weights"
-    )
-    os.makedirs(model_weights_dir, exist_ok=True)
-    weights_file_path = os.path.join(model_weights_dir, f"{model_name}_weights.joblib")
-    joblib.dump(model, weights_file_path)
+        # Save model weights
+        model_weights_dir = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "model_weights"
+        )
+        os.makedirs(model_weights_dir, exist_ok=True)
+        weights_file_path = os.path.join(
+            model_weights_dir, f"{model_name}_weights.joblib"
+        )
+        joblib.dump(model, weights_file_path)
+
+    except Exception as e:
+        logging.error("\nError occurred while fitting the Adaboost model.")
+        logging.exception(e)
